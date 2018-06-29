3ec73af0e59533c8ededda3fe60e250e1bd627a2
junhao1996 <1258884157@qq.com>
2018-06-29 16:40:52 +0800

    changed by junhao 6.28

---
+++ t1.py
@@ -0,0 +1,34 @@
+import re
+
+import requests as req
+# print(r.statu;s_code)
+# for i in r.history:
+#     print(i.url)
+#     print(i.status_code)
+
+def get_html(url):
+    r = req.get(url)
+    print(type(r))
+    # print(r.text)
+    return r.text
+
+
+
+def get_url(html):
+    # q = re.compile('a href=(.+?)">', re.S)
+    q = re.compile(r'(?<=href=\").*?(?=\")', re.S)
+    a = re.findall(q, html)
+    print(len(a))
+
+    return a
+
+
+if __name__ == "__main__":
+    res = get_html("https://www.douban.com/")
+    res_url = get_url(res)
+    print(res_url)
+    dict1 = {}
+    for i,j in enumerate(res_url):
+        dict1[i] = j
+    print(dict1)
+
