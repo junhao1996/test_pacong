e4c5d28a2ea07fd06a45507688359c7b2a8eda73
junhao1996 <1258884157@qq.com>
2018-06-30 11:59:02 +0800

    使用Worker 线程池对爬虫进行优化

--- pachong.py
+++ pachong.py
@@ -1,17 +1,30 @@
 import re
 import random
+import threading
+import time
+import threadpool
 import requests
 
+from worker.worker import Worker
 
-def get_picture():
-    url = "http://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&word=美女"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'}
-    res = requests.get(url, headers=headers)
-    a = re.compile(r'img src="(.*?)"', re.M)
-    result = re.findall(a, res.text)
 
-    print(res.text)
+def get_picture(data):
+    url = "http://616pic.com/category?md=1&p="+str(data)
+
+    r1 = requests.get(url)
+    a = re.compile(r'data-original="(.*?)"', re.M)
+    result = re.findall(a, r1.text)
+    for url in result:
+        local_name = url.split("/")[-1]
+        # print(local_name)
+
+        res = requests.get(url, stream=True)
+        # 用流的方式来写入图片
+        with open("/home/zhang/图片/"+local_name, "wb") as f:
+            for i in res.iter_content(chunk_size=1024):
+                f.write(i)
+
+    # print(res.text)
     print(result)
     # print(type(res.content))
     # data - objurl = "http://imgsrc.baidu.com/imgad/pic/item/10dfa9ec8a136327701bf8109b8fa0ec08fac71a.jpg"
@@ -54,7 +67,44 @@ def pachong():
             f.write(i)
 
 
+def work_do_sth(data, info):
+
+    for i in data:
+        get_picture(i)
+
+
 if __name__ == "__main__":
     # a = get_picture()
     # set(a)
-    find()
+
+    a = time.time()
+    data = [1, 2, 3, 4, 5, 6]
+    info = {
+        'worker': 'pachong.work_do_sth',
+        'chunk_size': 2
+    }
+    ww = Worker(mode="thread")
+    resp = ww.work(data, info)
+
+
+    # 直接爬取
+    # for i in data:
+    #     get_picture(i)
+
+    # for i in list(range(1,7)):
+    #     th1 = threading.Thread(target=get_picture, args=(i,))
+    #     th1.start()
+
+
+    
+    # th1 = threading.Thread(target=get_picture, args=(1,))
+    #
+    # th1.start()
+    # th2 = threading.Thread(target=get_picture, args=(2,))
+    # th2.start()
+    # th1.join()
+    # th2.join()
+    b = time.time()
+    print(b-a)
+    # 普通使用 29s
+    # 使用多线程14s
